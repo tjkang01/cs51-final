7370451071ebd5c05f99f959c4919d30ee1ddc93
Kevin Zhang <kevin.zhang500@gmail.com>
2015-04-17 06:50:01 -0400

    initial code document.py

--- document.py
+++ document.py
@@ -5,8 +5,14 @@ class Document:
     # if filename is None, initialize using text
     # if both are None, throw exception
     # text, filename are of type String
+    if text = None:
+      if filename = None:
+        throw exception
+      else self.text = open(filename, 'r').read()
+    else self.text = text
 
   # tokenizer
   def tokenize(del=" "):
     # tokenize the string using an optional delimiter
     # del is of type String
+    return self.text.split(del)
