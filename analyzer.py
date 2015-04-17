6d7cf0ba47be37c6c4d7294822c5c470b3e5077b
Kevin Zhang <kevin.zhang500@gmail.com>
2015-04-17 06:34:20 -0400

    Created initial py files

---
+++ analyzer.py
@@ -0,0 +1,25 @@
+class Analyzer:
+  # utility class, hidden from users
+  class Heuristic:
+    # initializer
+    def __init__(self, opt, text):
+      # initialize the Heuristic using the heuristic’s name (opt) and combined text (text) of all of the documents in the seed 
+      # opt, text are of type String
+    # updater
+    def update(self, doc):
+      # update the stored values with a new document
+      # doc is of type Document
+    # initializer
+    def __init__(self, seed):
+      # initialize the object
+      # seed is of type Seed
+    # updater
+    def update(self, opt, doc):
+      # update stored values using a new doc
+      # opt is of type String
+      # doc is of type Document
+    # analyzer
+    def analyze(self, doc):
+      # analyze a new document using the stored values
+      # needs to output the probabilities of each of the stored heuristics, ranked from highest to lowest probability
+      # doc is of type Document
---
+++ document.py
@@ -0,0 +1,12 @@
+class Document:
+  # initializer
+  def __init__(self, text=None, filename=None):
+    # if text is None, initialize using filename
+    # if filename is None, initialize using text
+    # if both are None, throw exception
+    # text, filename are of type String
+
+  # tokenizer
+  def tokenize(del=" "):
+    # tokenize the string using an optional delimiter
+    # del is of type String
