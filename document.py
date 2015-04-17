be2c7e782391a91563480d623353a58e1848a357
Kevin Zhang <kevin.zhang500@gmail.com>
2015-04-17 07:16:23 -0400

    Finished document class and started bintree

--- document.py
+++ document.py
@@ -1,3 +1,6 @@
+class NO_TEXT_FOUND(Exception):
+  pass
+
 class Document:
   # initializer
   def __init__(self, text=None, filename=None):
@@ -5,14 +8,14 @@ class Document:
     # if filename is None, initialize using text
     # if both are None, throw exception
     # text, filename are of type String
-    if text = None:
-      if filename = None:
-        throw exception
-      else self.text = open(filename, 'r').read()
-    else self.text = text
+    if text == None:
+      if filename == None:
+        raise NO_TEXT_FOUND("Neither text nor file found")
+      else: self.text = open(filename, 'r').read()
+    else: self.text = text
 
   # tokenizer
-  def tokenize(del=" "):
+  def tokenize(self, delimiter=' '):
     # tokenize the string using an optional delimiter
     # del is of type String
-    return self.text.split(del)
+    return self.text.split(delimiter)
