f313b885c719a3356fe5b6a293c6a9821d60941d
Kevin Zhang <kevin.zhang500@gmail.com>
2015-04-20 00:45:18 -0400

    Added hashtable interface

---
+++ hashtable.py
@@ -0,0 +1,12 @@
+class HashTable:
+  # initializer
+  def __init__(self):
+    # create initial hashtable with all empty entries (e.g. 28 HashTable array with zeroed entries)
+    # entries should be of any type (we will be using ints and floats)
+
+  # accessor methods
+  def get(self, n):
+    # return nth entry (e.g. HashTable for nth letter)
+
+  def add(self, val=None):
+    # method to add in a new value to the HashTable (e.g. a new word)
