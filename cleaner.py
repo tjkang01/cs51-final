b13f78f1e635df84a895c76d9ebd4d1af9a616fa
Timothy Kang <TimothyKang@Timothys-MacBook-Air-2.local>
2015-04-23 22:41:31 -0400

    Added cleaner file

---
+++ cleaner.py
@@ -0,0 +1,27 @@
+from util import Util
+import re
+import string
+
+def format(file):
+  # review is the name of the file that will be cleaned
+  # open the review file
+  f = open(file, "r")
+  # begin reading
+  # number of reviews
+  number = 0
+  for line in f:
+    # only gets the actual review
+    if "review/text" in line:
+      # increments
+      number += 1
+      # gets rid of the review
+      temp = line.replace("review/text:", "")
+      # turns the review into a tokenized list
+      words = Util.tokenize(temp)
+      # creates new file with incremented number
+      fi = open(str(number) + ".txt", "w")
+      for word in words:
+        # only adds words that meet the follow regular pattern
+        if (re.compile("^[a-zA-Z'-]+$")).match(word):
+          fi.write(word + " ")
+      fi.close()
